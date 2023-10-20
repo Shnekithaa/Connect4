5cb7c1d4071f3e388eb673ed33e4e5fa45b02dbd
BALASHNEKITHAA SATHEESHKUMAR <shnekithaasatheesh2004@gmail.com>
2023-10-20 11:06:34 +0530

    Completed

--- gameover.js
+++ gameover.js
@@ -4,6 +4,7 @@ let playerDetails = document.getElementById("playerDetails");
 let playAgainBtn = document.getElementById("playAgainBtn");
 let tie = document.getElementById("tie");
 let heading = document.getElementById("heading");
+let homeIcon = document.getElementById("homeIcon");
 
 let commentsArr = ["Awesome!!", "Good Job!!", "Almost There!!"];
 
@@ -21,15 +22,8 @@ playAgainBtn.onclick = function(){
     location.href = "game.html";
 }
 
-function createAndAppendPlayer(name, coins){
-    let playerName = document.createElement('p');
-    playerName.id = "leaderBoardPlayer";
-    playerName.textContent = name;
-    playerDetails.appendChild(playerName);
-
-    let numberOfCoins = document.createElement('p');
-    numberOfCoins.textContent = coins.toString() + " coins" ;
-    playerDetails.appendChild(numberOfCoins);
+homeIcon.onclick = function(){
+    location.href = "index.html";
 }
 
 
@@ -37,17 +31,52 @@ let winner = localStorage.getItem("winner");
 
 if(winner == "Player1"){
     playerWon.textContent = localStorage.getItem("player1");
-    createAndAppendPlayer(localStorage.getItem("player1"), localStorage.getItem("redCoinCount"));
+    addPlayerToLeaderBoard(localStorage.getItem("player1"), localStorage.getItem("redCoinCount"));
     decideComments(localStorage.getItem("redCoinCount"));
 }else if(winner == "Player2"){
     playerWon.textContent = localStorage.getItem("player2");
-    createAndAppendPlayer(localStorage.getItem("player2"), localStorage.getItem("yellowCoinCount"));
+    addPlayerToLeaderBoard(localStorage.getItem("player2"), localStorage.getItem("yellowCoinCount"));
     decideComments(localStorage.getItem("yellowCoinCount"));
 }else{
     playerWon.textContent = "It's a tie!!";
     tie.style.display = "none";
 }
 
-window.onload = function(){
-    localStorage.setItem("playerCoins", JSON.stringify(playersArr));
+
+function addPlayerToLeaderBoard(player, coins){
+    let playerData = JSON.parse(localStorage.getItem('leaderboard')) || [];
+
+    const existingPlayer = playerData.find((entry) => entry.player === player);
+    if (existingPlayer) {
+        existingPlayer.coins = coins;
+    } else {
+        playerData.push({ player, coins });
+    }
+
+    playerData.sort((a, b) => a.coins - b.coins);
+
+    localStorage.setItem('leaderboard', JSON.stringify(playerData));
+
+    displayLeaderboard(playerData);
 }
+
+function displayLeaderboard(playerData){
+    playerDetails.innerHTML = "";
+    playerData.forEach((playerObj) => {
+        let div = document.createElement('div');
+        div.style.display = "flex";
+        div.style.justifyContent = "space-between";
+        playerDetails.appendChild(div);
+
+        let para1 = document.createElement('p');
+        para1.textContent = playerObj.player;
+
+        let para2 = document.createElement('p');
+        para2.textContent = playerObj.coins;
+
+        div.appendChild(para1);
+        div.appendChild(para2);
+    });
+}
+
+// localStorage.clear()
